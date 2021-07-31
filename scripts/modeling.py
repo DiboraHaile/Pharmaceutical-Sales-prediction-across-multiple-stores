32ade2695d0380b8cbb233a060bbbbfe65839f09
DiboraHaile <deborahaile3@gmail.com>
2021-07-31 14:38:21 +0300

    run regression model with mlflow tracking

--- scripts/modeling.py
+++ scripts/modeling.py
@@ -4,12 +4,15 @@ import sys
 import seaborn as sn
 import matplotlib.pyplot as plt
 import numpy as np
-import clean_data
+# import clean_data
 import loading_data
-import utilities
-from sklearn.neighbors import LocalOutlierFactor
-from sklearn import tree
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
 import mlflow
 import mlflow.sklearn
 import logging
@@ -30,6 +33,69 @@ def load_versions(path,version):
 
     return df,data_url
 
+# class to include custom functions on dataframes
+# in the pipeline
+class df_function_transformer():
+    def __init__(self, func):
+        self.func = func
+
+    def transform(self, input_df, **transform_params):
+        return self.func(input_df)
+
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+# function to create more features from Date columns
+def get_features(df_train): 
+    # extracting numerical information from the date columns
+    # the year
+    df_train_copy = df_train.copy()
+    df_train_copy["Year"] = df_train_copy['Date'].dt.year
+    # which part of the month it is where 0 is begining, 1 is mid and 2 is end
+    df_train_copy["Part of the month"] = df_train_copy['Date'].dt.day.apply(lambda x: x // 10)
+    df_train_copy.loc[(df_train_copy["Date"].dt.day == 31), "Part of the month"] = 2
+    df_train_copy = df_train_copy.drop(columns="Date")
+    # How many days before or after holidays
+    return df_train_copy
+    
+
+# function to convert to dataframe
+def format_datetime(df):
+    df["Date"] = pd.to_datetime(df["Date"])
+    return df
+
+
+def prepare_model_input(df):
+    df_y = df["Sales"]
+    df_X = df.drop(columns=["Sales"])
+    return df_X, df_y
+
+# preprocess data
+def preprocess(df):
+    categorical_preprocessing = Pipeline([('ohe', OneHotEncoder())])
+    numerical_preprocessing = Pipeline([('imputation', SimpleImputer())])
+
+    # define which transformer applies to which columns
+    preprocess = ColumnTransformer([
+        ('categorical_preprocessing', categorical_preprocessing, ['StateHoliday']),
+        ('numerical_preprocessing', numerical_preprocessing, ['Store', 'DayOfWeek','Customers', 'Open', 'Promo'
+            ,'SchoolHoliday'])
+    ])
+    training_pipeline = Pipeline([
+        ("convert_Date_format", df_function_transformer(format_datetime)),
+        ("get features from Date",df_function_transformer(get_features)),
+        ("encode and impute", preprocess),
+        ("Scale",StandardScaler())
+        
+    ])
+    return training_pipeline.fit_transform(df)
+
+def train_model(X,y,model):
+    reg = model.fit(X, y)
+    return reg
+
+def inference_model(X,model):
+    return model.predict(X)
 
 def ml_pipeline():
     
@@ -46,23 +112,11 @@ def ml_pipeline():
         mlflow.log_param('input_rows', df_train.shape[0])
         mlflow.log_param('input_cols', df_train.shape[1])
 
-        # partition data to features and target
-        df_train = utilities.format_datetime(df_train,"Date")
-        # df_test = utilities.format_datetime(df_train,"Date")
-        # cleaned_test = clean_data.Handle_missing_values(df_test,drop_cols=False,drop_rows=False)
-
-        # removing date and get only year value
-        df_train_copy = df_train.copy()
-        df_train_copy["Year"] = df_train_copy['Date'].dt.year
-        df_training = df_train_copy.drop(columns='Date')
-        # encode state holidays to numbers
-        holidays = {'0': 0, 'a': 1, 'b':2, 'c':3}
-        df_training["StateHoliday"] = df_training['StateHoliday'].map(lambda x: holidays[x])
-        y_df = df_training['Sales']
-        x_df = df_training.drop(columns='Sales')
+        # get features and target
+        df_features,df_target = prepare_model_input(df_train)
 
         #log artifacts: columns used for modeling
-        cols_x = pd.DataFrame(list(x_df.columns))
+        cols_x = pd.DataFrame(list(df_features.columns))
         cols_x.to_csv('features.csv', header=False, index=False)
         mlflow.log_artifact('features.csv')
 
@@ -70,15 +124,19 @@ def ml_pipeline():
         cols_y.to_csv('target.csv', header=False, index=False)
         mlflow.log_artifact('target.csv')
 
-        #run models and store parameters
+        y = np.array(df_target)
+        X = preprocess(df_features)
+        # split into valid and training data
+        X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=.3, random_state=12)
         
-        y_train = np.array(y_df)
-        X_train = np.array(x_df)
+        # train with training data
+        reg = LinearRegression()
+        trained_model = train_model(X_train,y_train,reg)
+        #run models and store parameters
+        score = trained_model.score(X_valid, y_valid)
+        print("The score of the trained Linear regression model is ",score)
+        mlflow.log_param('Score of model', score)
 
-        scaler = MinMaxScaler()
-        X_train = scaler.fit_transform(X_train)
-        # clf_decisions = tree.DecisionTreeClassifier()
-        # decision_clf_trained = clf_decisions.fit(X_train,y_train)
 
 if __name__ == "__main__":
     ml_pipeline()
